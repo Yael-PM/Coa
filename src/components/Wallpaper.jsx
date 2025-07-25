2f7eb4b43e57f07b1396b7c95193811030315406
Yael Octavio Perez Mendez <yaelpm.dev08@Gabriels-MacBook-Air.local>
2025-07-24 20:50:15 -0600

    Deploy del proyecto

---
+++ src/components/Wallpaper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Question from './Question';
+
+function Wallpaper() {
+    return (
+        <div className='bg-pink-400 h-[100vh] w-full flex justify-center items-center'>
+            <Question></Question>
+        </div>
+    );
+}
+
+export default Wallpaper;
